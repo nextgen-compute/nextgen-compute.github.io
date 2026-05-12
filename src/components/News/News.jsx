145a3a837d34818ece128f59ac024a66ad8e3aa5
nextgen-compute <emergingcomputing@gmail.com>
2026-05-12 12:48:47 -0500

    Update News.jsx

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -3,7 +3,6 @@ import styles from './News.module.css';
 
 const news = [
     { title: "March 2026", description: "Papers got accepted at MDTS'26 and DCAS'26" },
-    { title: "January 2026", description: "Congratulations to Sanvi on receiving a National Honorable Mention for the NCWIT Aspirations in Computing High School Award!" },
     { title: "February 2025", description: "One paper got accepted at MDTS'25." },
     { title: "January 2025", description: "One paper got accepted in ACM TODAES." },
     { title: "June 2024", description: "One paper got accepted at ICCAD'24." },
